f431d09b21ae7a664454ea3c68ec71bba534ad77
Agustin Groh <agusgroh@gmail.com>
2022-09-19 17:19:11 -0300

    Adds csv reader

--- arqui-entregable-1/src/main/java/Entities/Client.java
+++ arqui-entregable-1/src/main/java/Entities/Client.java
@@ -1,4 +1,4 @@
-package Entidades;
+package Entities;
 
 public class Client {
     private int id;
@@ -7,9 +7,15 @@ public class Client {
 
     private String mail;
 
+
     public Client(String name, String mail) {
         this.name = name;
         this.mail = mail;
+    }
+
+    public Client (Integer id, String name, String mail){
+        this(name,mail);
+        this.id= id;
     }
 
     public int getId() {
@@ -34,6 +40,11 @@ public class Client {
 
     public Client getClient(){
         return new Client(this.name, this.mail);
+    }
+
+    @Override
+    public String toString(){
+        return  "Id:" + this.id + " " + "Name:" + this.name + " " + "Email:" + this.mail;
     }
 }
 
