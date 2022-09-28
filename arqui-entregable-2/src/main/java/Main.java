043de9a756186294e1dc4885bd2b45220b77f7b5
Agustin Groh <agusgroh@gmail.com>
2022-09-28 16:43:06 -0300

    Agrega persistance.xml e Hibernate

---
+++ arqui-entregable-2/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import entities.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("arqui");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Person p = new Person(123456,"test");
+
+        em.persist(p);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+*/
+
+
+    }
+}
