3b03fb14cfbf17095cc821fa9a9f142a270f14f3
Agustin Groh <agusgroh@gmail.com>
2022-10-12 16:33:21 -0300

    Agrega entregable3

---
+++ entregable3/src/main/java/com/example/entregable3/controller/PersonController.java
@@ -0,0 +1,16 @@
+package com.example.entregable3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+  //  @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+
+}
