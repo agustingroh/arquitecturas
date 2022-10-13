8a2efbdc6d9fb137339f8736a48cef2d81b006ae
Agustin Groh <agusgroh@gmail.com>
2022-10-13 07:35:09 -0300

    Agrega repositorio persona

---
+++ entregable3/src/main/java/com/arqui/entregable3/service/PersonService.java
@@ -0,0 +1,30 @@
+package com.arqui.entregable3.service;
+
+import com.arqui.entregable3.entity.Person;
+import com.arqui.entregable3.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll(){
+        List < Person > students = new ArrayList< >();
+        List < Person > studentList = personRepository.findAll();
+        studentList.forEach(person -> students.add(person));
+        return students;
+    }
+
+
+}
