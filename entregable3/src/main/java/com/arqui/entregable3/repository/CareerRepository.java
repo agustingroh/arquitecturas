914ba92ff28b5915886f24438defbebf63c45fee
Agustin Groh <agusgroh@gmail.com>
2022-10-14 18:42:47 -0300

    Obtiene carreras con estudiantes inscriptos

--- entregable3/src/main/java/com/arqui/entregable3/repository/CareerRepository.java
+++ entregable3/src/main/java/com/arqui/entregable3/repository/CareerRepository.java
@@ -1,9 +1,12 @@
 package com.arqui.entregable3.repository;
-
 import com.arqui.entregable3.entity.Career;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 public interface CareerRepository extends JpaRepository<Career, Integer> {
+    @Query("SELECT DISTINCT c FROM Career c JOIN c.students s WHERE size(s) > 0 ORDER BY size(s) DESC")
+    List<Career> getAllCareersWithStudents();
 }
