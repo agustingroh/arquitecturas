3579d55b7ca5e23ae75dcd2b2a9eed6b0fc8c84b
Agustin Groh <agusgroh@gmail.com>
2022-10-14 18:42:47 -0300

    Obtiene carreras con estudiantes inscriptos

--- entregable3/src/main/java/com/arqui/entregable3/service/CareerService.java
+++ entregable3/src/main/java/com/arqui/entregable3/service/CareerService.java
@@ -1,16 +1,23 @@
 package com.arqui.entregable3.service;
 
+import com.arqui.entregable3.entity.Career;
 import com.arqui.entregable3.repository.CareerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CareerService {
 
-    private CareerRepository CareerRepository;
+    private CareerRepository careerRepository;
 
     @Autowired
     public CareerService(CareerRepository CareerRepository){
-        this.CareerRepository = CareerRepository;
+        this.careerRepository = CareerRepository;
+    }
+
+    public List<Career> getAllCareersWithStudents(){
+        return this.careerRepository.getAllCareersWithStudents();
     }
 }
